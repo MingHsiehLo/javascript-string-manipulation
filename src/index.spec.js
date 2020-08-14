a55a3a9ee70fa022196464b4c5c3d43f1e505266
Ming <minghsiehlo@gmail.com>
2020-08-14 16:15:49 -0500

    fixed asserts

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const { assets } = require("chai");
+const { assert } = require("chai");
 const { describe, it } = require("mocha");
 const { lf } = global.lf || require("index.js");
 
@@ -7,18 +7,17 @@ describe("CRLF Converter", () => {
   describe("lf", () => {
     it("should replace CRLF at the beginning of a string", () => {
       const result = lf`\r\nHello there.`;
-      assets(result.startsWith('\nHello'));
+      assert(result.startsWith('\nHello'));
     });
 
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
-      assets(result.endsWith('there.\n'));
+      assert(result.endsWith('there.\n'));
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
-      assets(result.includes('o \n\nt'))
-      // Use `String.prototype.includes()`
+      assert(result.includes('o \n\nt'))
     });
   });
 });
